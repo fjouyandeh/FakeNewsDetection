ed03380e441bd45e33e9060038ea637e05dd2fbe
Farzaneh <far.jouyandeh@gmail.com>
2021-03-14 21:33:44 -0400

    Non-English rows are removed, Tokenization script is added

---
+++ Tokenization.py
@@ -0,0 +1,51 @@
+import nltk
+import pandas as pd
+import re
+
+dataset = pd.read_csv("./Dataset/additionalFeatureData.csv" , encoding = "ISO-8859-1")
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preproces(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+dataset['tokenized_text']= dataset['text'].apply(preproces)
+
+# Download english stopwords from nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# Removes stopwords
+stop_words = set(stopwords.words('english'))
+dataset['token_texts'] = dataset['tokenized_text'].apply(lambda x : [w for w in x if w.lower() not in stop_words])
+
+dataset.to_csv("./Dataset/FinalDS_AdditionalFeatures.csv")
